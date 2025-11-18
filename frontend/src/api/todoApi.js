486ea730aa43bba6b253eb721195757aad59bc71
shivansnigam <142572162+shivansnigam@users.noreply.github.com>
2025-11-18 17:18:21 +0530

    Fix netlify build and update config

--- frontend/src/api/todoApi.js
+++ frontend/src/api/todoApi.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 let api = axios.create({
-  baseURL: "http://localhost:5000/api/v1/todos"
+  baseURL: "https://todo-management-system-wheat.vercel.app/api/v1/todos"
 });
 
 // GET todos with optional filters/search
